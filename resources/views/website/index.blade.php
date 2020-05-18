c7d40e4ae9a788f07a6984e5c319b493f1bcadc9
Maria Jéssica Souza Pimentel <mjessicasp@gmail.com>
2020-05-18 15:32:35 -0300

    About the project

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -20,21 +20,18 @@
             </div>
         </div>
         <div class="row">
+            <!-- Texto -->
+            <div class="col-12 col-md-5 offset-md-1 my-auto">
+                <p class="text-justify">
+                    Este é um projeto que visa ensinar e educar sobre o lixo. "Ecodoor" foi uma súbita ideia sobre a porta para uma vida mais ecológica. Para algumas pessoas lixo é algo banal, mas sempre me incomodou ver desde um sofá jogado na avenida até um papel no chão.
+                </p>
+                <p class="text-justify">
+                    Por isso, pensei em juntar todo o conteúdo em um único lugar. Contar as vantagens de uma cidade mais limpa, mostrar os pontos da cidade onde tem as coletas seletivas, ensinar sobre a reciclagem do lixo e como ela é importante para nós, para nossas futuras gerações e para o planeta como um todo.
+                </p>
+            </div>
             <!-- Img -->
             <div class="col-12 col-md-6 text-center my-auto">
-                <img class="img-fluid " src="https://via.placeholder.com/500x350" alt="imagem de fundo">
-            </div>
-            <!-- Texto -->
-            <div class="col-12 col-md-5 offset-md-1">
-                <p class="text-justify">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis lobortis risus eget aliquam. Proin molestie tortor vel lectus viverra, sit amet interdum erat rutrum. Nulla vel dui massa. Proin iaculis ornare tempus. Etiam felis massa, interdum vitae lectus vitae, interdum imperdiet quam. Morbi suscipit diam sit amet purus faucibus posuere.
-                </p>
-                <p class="text-justify">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis lobortis risus eget aliquam. Proin molestie tortor vel lectus viverra, sit amet interdum erat rutrum. Nulla vel dui massa. Proin iaculis ornare tempus. Etiam felis massa, interdum vitae lectus vitae, interdum imperdiet quam. Morbi suscipit diam sit amet purus faucibus posuere.
-                </p>
-                <p class="text-justify">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis lobortis risus eget aliquam. Proin molestie tortor vel lectus viverra, sit amet interdum erat rutrum. Nulla vel dui massa. Proin iaculis ornare tempus. Etiam felis massa, interdum vitae lectus vitae, interdum imperdiet quam.
-                </p>
+                <img class="img-fluid " src="{{ asset('img/sobre.jpg') }}" alt="imagem de fundo" style="max-height: 285px">
             </div>
         </div>
     </section>
@@ -47,9 +44,9 @@
                     <h2 class="section-title">Objetivos</h2>
                 </div>
             </div>
-            <div class="row mt-5">
+            <div class="row mt-3">
                 <div class="col-12 col-md-6 text-center my-auto">
-                    <h4 class="section-subtitle">Preservar o ambiente para as gerações futuras</h4>
+                    <h4 class="section-subtitle">Educação ambiental</h4>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis lobortis risus eget aliquam. Proin molestie tortor vel lectus viverra, sit amet interdum erat rutrum. Nulla vel dui massa. Proin iaculis ornare tempus. Etiam felis massa, interdum vitae lectus vitae
                     </p>
@@ -72,7 +69,7 @@
             </div>
             <div class="row mt-5">
                 <div class="col-12 col-md-6 text-center my-auto">
-                    <h4 class="section-subtitle">Educação ambiental</h4>
+                    <h4 class="section-subtitle">Preservar o ambiente para as gerações futuras</h4>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis lobortis risus eget aliquam. Proin molestie tortor vel lectus viverra, sit amet interdum erat rutrum. Nulla vel dui massa. Proin iaculis ornare tempus. Etiam felis massa, interdum vitae lectus vitae
                     </p>
